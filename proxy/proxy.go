3557185a595fe231655e2e8f8b8e3273743f5ed4
Mark <mark.ernst.me@gmail.com>
2016-08-04 22:17:28 +0200

    Update proxy.go

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -13,10 +13,10 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/sammy007/open-ethereum-pool/policy"
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/policy"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/rpc"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/storage"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/util"
 )
 
 type ProxyServer struct {
