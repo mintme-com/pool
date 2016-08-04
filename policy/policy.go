b8a9a5dae9623d296df678a26eb40c33aaa565fe
Mark <mark.ernst.me@gmail.com>
2016-08-04 22:24:12 +0200

    Update policy.go

--- policy/policy.go
+++ policy/policy.go
@@ -9,8 +9,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/sammy007/open-ethereum-pool/storage"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/storage"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/util"
 )
 
 type Config struct {
