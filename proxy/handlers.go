64a222a63a4302028c295a237516de5beaa713c2
Mark <mark.ernst.me@gmail.com>
2016-08-04 22:14:19 +0200

    Update handlers.go

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -5,8 +5,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/util"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/rpc"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
