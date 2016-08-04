0ac9bd47398acd5c1c45a0c5ed9150b595102726
Mark <mark.ernst.me@gmail.com>
2016-08-04 22:29:50 +0200

    Update unlocker_test.go

--- payouts/unlocker_test.go
+++ payouts/unlocker_test.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sammy007/open-ethereum-pool/rpc"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/rpc"
+	"github.com/LeChuckDE/open-ethereumclassic-pool/storage"
 )
 
 func TestMain(m *testing.M) {
