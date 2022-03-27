2ddcef7a54bc74d930b7d8eb24ce38d66a2e9754
Quackaddict <99321386+Quackaddict@users.noreply.github.com>
2022-03-27 09:32:35 -0500

    Update EliteCryptoPool.js

--- EliteCryptoPool.js
+++ EliteCryptoPool.js
@@ -1,1 +1,5 @@
 -o pool.webchain.network:3333 -u <0x2320f504543b5cb2ae98497bb5cb7dd29c8ccc6e> -p x
+webchain-miner -o 
+pool.webchain.network:3333 -u 
+0x2320f504543b5cb2ae98497bb5cb7dd29c8ccc6e 
+-p 0x
